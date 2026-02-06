2f29331620fb5205b6312e5b7ff76bd78aba2333
Andres-Vazquez-Leon <andres.vazquezleon01@gmail.com>
2026-02-05 18:47:31 -0700

    Se creo la clase Sport con un constructor

---
+++ game_tournament/Sport.py
@@ -0,0 +1,5 @@
+class Sport:
+    """Sport class represents a sport in the pturnament. It has name and league"""
+    def __init__(self, name, numplayers, league):
+        self.name = name
+        self.league = league
