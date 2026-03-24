b308ac9b34dce0dd522a7f1840b41090f61fadbb
Andres-Vazquez-Leon <andres.vazquezleon01@gmail.com>
2026-03-23 18:51:02 -0700

    se creo un proyecto para una libreria

---
+++ books/book_funtions.py
@@ -0,0 +1,36 @@
+"""book_funtions.py
+conteins functions to work with books objects"""
+
+from Book import Book
+from Book import load_books
+
+def get_genres(books:list[Book])->list[str]:
+    """Get all unique genres from the list of books objects"""
+    genres = set()
+    for book in books:
+        genres.add(book.genre)
+    return sorted(genres)
+
+def create_author_dictionary(books:list[Book])->dict[str, list[Book]]:
+    """Create a dictionary with authors as keys and their books"""
+    author_dict = {}
+    for book in books:
+        if book.author.lower() not in author_dict:
+            author_dict[book.author.lower()] = []
+        author_dict[book.author.lower()].append(book)
+        # Multiple names or authors
+        author_names = book.author.lower().split(" ")
+        if len(author_names) >= 2:
+            for name in author_names:
+                if name not in author_dict:
+                    author_dict[name] = []
+                author_dict[name].append(book)
+    return author_dict
+            
+"""Hacer busquenda por titilo o por palabra del titulo"""
+            
+if __name__ == "__main__":
+    books = load_books("booklist2000.csv")
+    print(get_genres(books))
+    author_dict = create_author_dictionary(books)
+    print(author_dict["sandra"][0])
