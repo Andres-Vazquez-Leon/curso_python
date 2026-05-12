ad78428852dea728404340dd31236f4eeb66939a
Andres-Vazquez-Leon <andres.vazquezleon01@gmail.com>
2026-05-11 18:29:44 -0700

    UwU

--- frases_celebres/app.py
+++ frases_celebres/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, render_template, request
 from frases_celebres import Frase, carga_archivo_csv, crea_diccionario_titulos, buscar_palabras
+from frases_celebres.frases_celebres import buscar_palabras_ratio
 
 app = Flask(__name__)
 
@@ -17,6 +18,16 @@ def pelicula():
 @app.route('/frase')
 def frase():
     return render_template("frase.html")
+
+@app.route('/frase', methods=['GET', 'POST'])
+def buscar_frase():
+    if request.method == 'POST':
+        frase = request.form['frase']
+        umbral = request.form['umbral']
+        frases_encontradas = buscar_palabras_ratio(frases, frase, umbral)
+        return render_template("frase.html", frases=frases_encontradas)
+    else:
+        return render_template("frase.html", frases = frases)
 
 if __name__ == "__main__":
     app.run(debug=True) 
